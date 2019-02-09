0377bc8e11b09224a7284447ec16a93a4c3b7aa0
Geraldo Rabelo <geraldo.rabelo@gmail.com>
2019-02-08 21:52:48 -0300

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -20,3 +20,15 @@ app.post('/cpf', (req,res,next) => {
 	console.log(req.body.numero+'-'+primeiroDigito+''+segundoDigito);
 	res.send(req.body.numero+'-'+primeiroDigito+''+segundoDigito);
 });
+
+// Nono digito
+// 1. Distrito Federal, Goiás, Mato Grosso do Sul e Tocantins;
+// 2. Pará, Amazonas, Acre, Amapá, Rondônia e Roraima;
+// 3. Ceará, Maranhão e Piauí;
+// 4. Pernambuco, Rio Grande do Norte, Paraíba e Alagoas;
+// 5. Bahia e Sergipe;
+// 6. Minas Gerais;
+// 7. Rio de Janeiro e Espírito Santo;
+// 8. São Paulo;
+// 9. Paraná e Santa Catarina;
+// 0. Rio Grande do Sul.
